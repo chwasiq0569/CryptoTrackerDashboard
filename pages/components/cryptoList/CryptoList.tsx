a5204b4aeac902514fe1ea9ff3842d7be015e6d6
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-18 23:45:52 +0500

    Developed Currency Lsit

--- pages/components/cryptoList/CryptoList.tsx
+++ pages/components/cryptoList/CryptoList.tsx
@@ -1,4 +1,7 @@
-import styles from './CryptoList.module.css'
+import styles from './CryptoList.module.css';
+import Image from 'next/image';
+
+// const Icon = require('../../../assets/bitcoin_PNG48.png);
 
 export default function CryptoList(){
     return (
@@ -6,14 +9,123 @@ export default function CryptoList(){
             <div className={styles.individualCurrency}>
                 <div className={styles.leftSide}>
                     <div className={styles.iconContainer}>
-
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
                     </div>
-                    <div className={styles.currencyName}>
+                    <p className={styles.currencyName}>
                          BTC
-                    </div>  
+                    </p>  
                 </div>
                 <div className={styles.rightSide}>
                     <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.coinBtn}>
+                        <p>+</p>
+                    </div>
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>ADD COIN</p>
                 </div>
             </div>
         </div>
---
+++ pages/components/currencyInfo/CurrencyInfo.tsx
@@ -0,0 +1,10 @@
+import styles from './CurrencyInfo.module.css';
+import Image from 'next/image';
+
+export default function CryptoList(){
+    return(
+        <div className={styles.currencyInfo__Wrapper}>
+              
+        </div>
+    )
+}
