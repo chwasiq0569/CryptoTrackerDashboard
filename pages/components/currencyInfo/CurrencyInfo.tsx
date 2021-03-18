0941a395088b5f513fa24ad49110afd6d506d033
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-19 00:03:05 +0500

    Developed CurrenyInfoDetails

--- pages/components/currencyInfo/CurrencyInfo.tsx
+++ pages/components/currencyInfo/CurrencyInfo.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 export default function CryptoList(){
     return(
         <div className={styles.currencyInfo__Wrapper}>
-                <div className={styles.CurrencyInto}>
-                        <div className={styles.CurrencyInto__leftSide}>
+                <div className={styles.CurrencyInfo}>
+                        <div className={styles.CurrencyInfo__leftSide}>
                                 BTC
                         </div>
-                        <div className={styles.CurrencyInto__rightSide}>
+                        <div className={styles.CurrencyInfo__rightSide}>
                                 <div className={styles.imageContainer}>
                                     <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
                                 </div>
