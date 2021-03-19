1411b3d70f763d5b1a1c0f696507c3d0179b642b
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-03-20 00:50:26 +0500

    Fetched API

--- pages/components/cryptoList/CryptoList.tsx
+++ pages/components/cryptoList/CryptoList.tsx
@@ -3,10 +3,33 @@ import Image from 'next/image';
 
 // const Icon = require('../../../assets/bitcoin_PNG48.png);
 
-export default function CryptoList(){
+export default function CryptoList({ currencyList }){
+    console.log("CURRENCY LIST: ", currencyList)
     return (
         <div className={styles.outerContainer}>
-            <div className={styles.individualCurrency}>
+          <div className={styles.currencyListContainer}>
+                {
+                    currencyList.map(coin => (
+                        <div className={styles.individualCurrency}>
+                        <div className={styles.leftSide}>
+                            <div className={styles.iconContainer}>
+                                <img src={coin?.image} alt={coin.name} width="100%" height="100%" />
+                            </div>
+                            <p className={styles.currencyName}>
+                                 {coin.symbol}
+                            </p>  
+                        </div>
+                        <div className={styles.rightSide}>
+                            <p className={styles.price}>${coin.current_price}</p>
+                            {
+                                coin.price_change_percentage_24h < 0 ? <p className={styles.percentageRed}>{coin.price_change_percentage_24h.toFixed(2)}%</p> : <p className={styles.percentageGreen}>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                            }
+                        </div>
+                    </div>
+                    ))
+                }
+        
+            {/* <div className={styles.individualCurrency}>
                 <div className={styles.leftSide}>
                     <div className={styles.iconContainer}>
                         <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
@@ -118,6 +141,35 @@ export default function CryptoList(){
                     <p className={styles.percentage}>3.44%</p>
                 </div>
             </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div>
+            <div className={styles.individualCurrency}>
+                <div className={styles.leftSide}>
+                    <div className={styles.iconContainer}>
+                        <Image src="/bitcoin_PNG48.png" alt="me" width="100%" height="100%" />
+                    </div>
+                    <p className={styles.currencyName}>
+                         BTC
+                    </p>  
+                </div>
+                <div className={styles.rightSide}>
+                    <p className={styles.price}>$287.09</p>
+                    <p className={styles.percentage}>3.44%</p>
+                </div>
+            </div> */}
+          </div>
             <div className={styles.individualCurrency}>
                 <div className={styles.leftSide}>
                     <div className={styles.coinBtn}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import styles from '../styles/Home.module.css'
 import CryptoList from './components/cryptoList/CryptoList';
 import CurrencyInfo from './components/currencyInfo/CurrencyInfo';
 
-export default function Home() {
+export default function Home({ result }) {
+  console.log(result)
   return (
     <div className={styles.outerWrapper}>
       <div className={styles.innerWrapper}>
         <div className={styles.leftSide}>
-          <CryptoList />
+          <CryptoList currencyList={result} />
         </div>
         <div className={styles.middleSection}>
           Middle
@@ -18,5 +19,15 @@ export default function Home() {
         </div>
       </div>
     </div>
-  )
+  );
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+  const result = await response.json();
+  return {
+    props: {
+      result
+    }
+  }
 }
